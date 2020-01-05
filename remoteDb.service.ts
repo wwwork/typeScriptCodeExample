6d1defbe65bb100a4663a7381abe22f178a4f542
Max Shcerbakov <thewaygoesup@gmail.com>
2020-01-05 16:13:20 +0200

    Add files via upload

---
+++ remoteDb.service.ts
@@ -0,0 +1,65 @@
+import {BadRequestException, Injectable} from '@nestjs/common';
+import {Logger} from '../core/logger';
+import {RemoteDataBaseRepository} from '../repositories/remoteDataBase.repository';
+import {RemoteDb} from '../models/remoteDb.model';
+import {CacheService} from './cache.service';
+import {configInstance} from '../core/config';
+
+@Injectable()
+export class RemoteDbService {
+  constructor(
+    private remoteDbRepo: RemoteDataBaseRepository,
+    private logger: Logger,
+    private readonly redis: CacheService
+  ) {
+  }
+
+  // create temp  Union Tables
+  async createUnionTables(): Promise<void> {
+    try {
+      const tables = configInstance.remoteDb.tables;
+      for (const table in tables) {
+        await this.remoteDbRepo.createUnionTempTable(tables[table]);
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
+    }
+  }
+
+  // todo - add it to cron jobs
+  async checkIsRemoteAvailable(): Promise<boolean> {
+    try {
+      // check accounts main
+      const remoteTable = `${configInstance.remoteDb.tables.accounts}_main`;
+      await this.remoteDbRepo.countDataFromRemoteTable(remoteTable);
+      await this.redis.set(configInstance.remoteDb.redisName, '1', 3600, 'PX');
+      this.logger.info(`----=======## Remote database available ##==========-----`);
+      return true;
+    } catch (e) {
+      await this.redis.del(configInstance.remoteDb.redisName);
+      this.logger.error(`----=======## Remote database unavailable ##==========-----`);
+      return false;
+    }
+  }
+
+  // if we have data to move
+  // todo add it to cron jobs
+  async SynchronizeWithRemoteDb(): Promise<RemoteDb[]> {
+    try {
+      const remoteDbAvailable = this.redis.get(configInstance.remoteDb.redisName);
+      if (remoteDbAvailable) {
+        const recordsToMove = await this.remoteDbRepo.getAvailableDataForSynchronize();
+        if (recordsToMove.length) {
+          for (const item of recordsToMove) {
+            await this.remoteDbRepo.moveToRemoteTable(item);
+          }
+        }
+        return recordsToMove;
+      }
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
+    }
+  }
+}
